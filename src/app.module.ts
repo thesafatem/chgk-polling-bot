63b4be81f2bc388149be01bc69c21da36d0f64ad
thesafatem <thesafatem@gmail.com>
2023-04-05 03:03:37 +0600

    Add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose/dist';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChgkModule } from './chgk/chgk.module';
+import { getMongoConfig } from './configs/mongo.config';
 import { getTelegramConfig } from './configs/telegram.config';
 import { TelegramModule } from './telegram/telegram.module';
 
@@ -14,6 +16,11 @@ import { TelegramModule } from './telegram/telegram.module';
 			imports: [ConfigModule, ChgkModule],
 			inject: [ConfigService],
 			useFactory: getTelegramConfig,
+		}),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
 		}),
 	],
 	controllers: [AppController],
---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,25 @@
+import { ConfigService } from '@nestjs/config';
+import { MongooseModuleOptions } from '@nestjs/mongoose';
+
+export const getMongoConfig = async (
+	configService: ConfigService,
+): Promise<MongooseModuleOptions> => {
+	return {
+		uri: getMongoString(configService),
+	};
+};
+
+const getMongoString = (configService: ConfigService): string => {
+	return (
+		'mongodb://' +
+		configService.get('MONGO_LOGIN') +
+		':' +
+		configService.get('MONGO_PASSWORD') +
+		'@' +
+		configService.get('MONGO_HOST') +
+		':' +
+		configService.get('MONGO_PORT') +
+		'/' +
+		configService.get('MONGO_AUTH_DATABASE')
+	);
+};
