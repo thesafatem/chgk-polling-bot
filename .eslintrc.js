a04ea6551083a8c8edc0889c2a8f3fffffc20121
thesafatem <thesafatem@gmail.com>
2023-10-30 01:58:31 +0600

    Update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,12 @@ module.exports = {
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
-		'prettier/prettier': 2,
+		// 'prettier/prettier': 2,
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+			},
+		],
 	},
 };
